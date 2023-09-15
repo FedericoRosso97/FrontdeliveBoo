d4873493cb9c6cb7a82f6dedad0d87152ced8eeb
emanuelefarelli <emaaliens888@gmail.com>
2023-09-15 13:51:11 +0200

    Added router-view and scaffholding for scss

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import AppHeader from './components/AppHeader.vue';
+import AppFooter from './components/AppFooter.vue';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <AppHeader/>
+    <router-view>
+    </router-view>
+  <AppFooter/>
 </template>
 
 <style lang="scss">
   @import '../node_modules/bootstrap/scss/bootstrap';
+  @import './styles/general.scss';
 </style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
-import './style.scss'
+// import './style.scss'
+import { router } from './router';
 import App from './App.vue'
 
-createApp(App).mount('#app')
+createApp(App).use(router).mount('#app');
+// createApp(App).mount('#app')
+
