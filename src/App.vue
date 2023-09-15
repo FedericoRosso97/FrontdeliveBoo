82d357946d388fe936461d3eca6ac5c26c36c2c0
Ericson-Manalo <ericmana98@gmail.com>
2023-09-15 10:33:11 +0200

    added node_modules bootstrap

--- src/App.vue
+++ src/App.vue
@@ -15,16 +15,5 @@ import HelloWorld from './components/HelloWorld.vue'
 </template>
 
 <style lang="scss">
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  @import '../node_modules/bootstrap/scss/bootstrap';
 </style>
