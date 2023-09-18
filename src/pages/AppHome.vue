ad2f39a01f61463c65b1495dfe3983f735093990
emanuelefarelli <emaaliens888@gmail.com>
2023-09-18 19:30:39 +0200

    Added api call for typologies in AppHome

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -1,27 +1,84 @@
 <template>
-    <div>
-        <h1>
-            AppHome
-        </h1>
+    <div class="type-restaurant-title">
+        <h2>
+            Scegli il tipo di ristorante:
+        </h2>
+    </div>
+    <div class="restourant-types">
+        <ul>
+            <li v-for="typology in typologies">
+                <h3>
+                    {{ typology.name }}
+                </h3>
+            </li>
+        </ul>
     </div>
 </template>
 
 <script>
 import { store } from '../store';
+import axios from 'axios';
 
     export default{
         name: 'AppHome',
         data(){
             return{
                 store,
+                typologyApiUrl: 'http://127.0.0.1:8000/api/typology',
+                typologies: [],
+            }
+        },
+        methods:{
+            getTypology(){
+                axios.get(this.typologyApiUrl, {
+                    params: {
+                    }
+                })
+                    .then( (response) => {
+                        this.typologies = response.data.result;
+                        console.log(this.typologies);
+                     })
+                    .catch(function (error) {
+                        console.log(error);
+                    })
             }
         },
         mounted(){
             store.searchBar = true;
+            this.getTypology();
         }
     }
 </script>
 
 <style scoped lang="scss">
-    
+@use '../styles/partials/variables.scss' as *;
+    div.type-restaurant-title{
+        margin-top: 10rem;
+        h2{
+            text-align: center;
+        }
+    }
+
+    div.restourant-types{
+        margin: 0 auto;
+        width: 85%;
+        ul{
+            list-style-type: none;
+            display: flex;
+            justify-content: space-around;
+            flex-wrap: wrap;
+            li{
+                text-align: center;
+                width: 30%;
+                background-color: $BlueColor;
+                margin-top: 2rem;
+                padding: 1rem 0;
+                border-radius: 1rem;
+                border: 2px solid $LightBlueColor;
+                h3{
+                    color: white;
+                }
+            }
+        }
+    }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import {reactive} from 'vue';
 
 export const store = reactive({
-    logIn : false,
+    logIn : true,
     searchBar : true,
     advancedSearch : false,
 });
