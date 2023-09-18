11c0591ce6fe0ef8b1905eb9310e3508d020b403
Denise-Dichiara <dichiara.d@gmail.com>
2023-09-18 22:18:57 +0200

    Fixed Header and Footer

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,14 @@
 import AppHeader from './components/AppHeader.vue';
 import AppFooter from './components/AppFooter.vue';
 
+export default{
+    name : 'App',
+    components:{
+        AppFooter,
+        AppHeader,
+    }
+}
+
 </script>
 
 <template>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
-import './style.scss'
+//import './style.scss'
 //import { router } from './router';
 import App from './App.vue'
+
+
+//createApp(App).use(router).mount('#app')
 
 createApp(App).mount('#app')
