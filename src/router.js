5c3597fae6c14b76c8886320699b0090b03fc233
Denise-Dichiara <dichiara.d@gmail.com>
2023-09-18 21:15:10 +0200

    Imported pages

--- src/router.js
+++ src/router.js
@@ -1,2 +1,24 @@
 import { createRouter, createWebHistory } from "vue-router";
 
+//importo pagine
+import Restaurant from './pages/Restaurant.vue';
+import Edit from './pages/Edit.vue';
+
+
+const router = createRouter({
+    history:createWebHistory(),
+    routes:[
+        {
+            path: '/',
+            name: 'restaurant', 
+            component: Restaurant
+        },
+        {
+            path: '/edit',
+            name: 'edit', 
+            component: Edit
+        },
+    ]
+});
+
+export { router };
