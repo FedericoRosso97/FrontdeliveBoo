d54e509320732b42da4a8c06e63c8cf677a8ebc5
emanuelefarelli <emaaliens888@gmail.com>
2023-09-18 10:10:32 +0200

    Added initial scss rule to loginForm.vue

--- src/pages/LoginForm.vue
+++ src/pages/LoginForm.vue
@@ -1,35 +1,37 @@
 <template>
-    <div class="Login-container">
-        <form @submit.prevent="sendLoginForm">
-
-            <h2>
-                Effettua il login!
-            </h2>
-
-            <!-- EMAIL -->
-
-            <div class="form-group">
-                <label for="email">
-                    Inserisci la tua email:
-                </label>
-                <input type="email" v-model="email">
-            </div>
-
-            <!-- PASSWORD -->
-
-            <div class="form-group">
-                <label for="password">
-                    Inserisci la tua password:
-                </label>
-                <input type="text" v-model="password">
-            </div>
-
-            <div class="submit-button">
-                <button type="submit">
-                    Login!
-                </button>
-            </div>
-        </form>
+    <div class="page-container">   
+        <div class="login-container">
+            <form @submit.prevent="sendLoginForm">
+                
+                <h2>
+                    Effettua il login!
+                </h2>
+                
+                <!-- EMAIL -->
+                
+                <div class="form-group">
+                    <label for="email">
+                        Inserisci la tua email:
+                    </label>
+                    <input type="email" v-model="email">
+                </div>
+                
+                <!-- PASSWORD -->
+                
+                <div class="form-group">
+                    <label for="password">
+                        Inserisci la tua password:
+                    </label>
+                    <input type="text" v-model="password">
+                </div>
+                
+                <div class="submit-button">
+                    <button type="submit">
+                        Login!
+                    </button>
+                </div>
+            </form>
+        </div>
     </div>
 </template>
 
@@ -64,5 +66,16 @@ export default {
 </script>
 
 <style scoped lang="scss">
+
+    div.page-container{
+        position: absolute;
+        top:0;
+        height: 100vh;
+        width: 100vw;
+        background-color: rgba(0, 0, 0, 0.742);
+        div.login-container{
+            
+        }
+    }
     
 </style>
