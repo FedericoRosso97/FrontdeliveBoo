7fadd52fff5997030448c7ef268035ca3a672d37
Denise-Dichiara <dichiara.d@gmail.com>
2023-09-19 20:10:20 +0200

    Modified AppHeader, centered element

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,27 +4,24 @@
     </div>
 
     <div class="lower-header">
-        <div class="choose-box">
-            <h1>
-                Cosa vuoi vedere?
-            </h1>
+        <div class="searchBar">
+            <div class="choose-box">
+                <h1>
+                    Cosa vuoi vedere?
+                </h1>
+            </div>
+
+            <nav>
+                <ul>
+                    <li v-for="link in links">
+                        <router-link :to="{ name: link.route }">
+                            {{ link.name }}
+                        </router-link>
+                    </li>
+                </ul>
+            </nav>
         </div>
-        <nav>
-            <ul>
-                <li v-for="link in links">
-                    <router-link :to="{ name: link.route }">
-                        {{ link.name }}
-                    </router-link>
-                </li>
-            </ul>
-        </nav>
     </div>
-
-
-
-
-
-
 </template>
 
 <script>
@@ -51,7 +48,7 @@ export default {
 <style scoped lang="scss">
 .upper-header {
     background-color: #1695A6;
-    height: 30%;
+    height: 150px;
     display: flex;
     justify-content: space-between;
     align-items: center;
@@ -67,31 +64,47 @@ nav {
     width: 100%;
     display: flex;
     justify-content: center;
+}
 
+ul {
+    display: flex;
+    font-size: 2rem;
+    list-style-type: none;
+}
 
-    ul {
-        display: flex;
-        font-size: 2rem;
-        list-style-type: none;
-    }
+li {
+    margin-right: 2rem;
 
-    li {
-        margin-right: 2rem;
-
-        a {
-            text-decoration: none;
-            color: blue
-        }
+    a {
+        text-decoration: none;
+        color:white;
     }
 }
 
 .lower-header {
-    background-color:#FDCF48;;
-    height: 100px;
-}
-.choose-box{
+    background-color: #FDCF48;
+    height: 150px;
     display: flex;
     justify-content: center;
-}
 
+
+    .choose-box {
+        display: flex;
+        justify-content: center;
+
+        h1 {
+            color: #F8F8F8;
+        }
+    }
+}
+.searchBar {
+    width: 50%;
+    padding: 2.5rem;
+    background-color: #24B0C1;
+    border: 7px solid white;
+    border-radius: 2rem;
+    position: absolute;
+    top: 200px;
+    text-align: center;
+}
 </style>
