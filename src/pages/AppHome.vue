2e822c84492694a79a156699889f480a9db552bd
FedericoRosso97 <federicorosso97@gmail.com>
2023-09-26 15:39:14 +0200

    fix restaurants/typologies

--- src/pages/AppHome.vue
+++ src/pages/AppHome.vue
@@ -24,7 +24,7 @@ import axios from 'axios';
         data(){
             return{
                 store,
-                typologyApiUrl: 'http://127.0.0.1:8000/api/typology',
+                typologyApiUrl: 'http://127.0.0.1:8000/api/typologies',
                 typologies: [],
             }
         },
